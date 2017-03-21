cbca6902d7d65dd11984a9356f3f0e56f160afa8
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-03-21 14:18:14 +0200

    Fixed themes issues

--- Folquest/app/src/main/java/com/kantele/folquest/MainActivity.java
+++ Folquest/app/src/main/java/com/kantele/folquest/MainActivity.java
@@ -1,13 +1,49 @@
 package com.kantele.folquest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonAvatar;
+    Button buttonQuests;
+    Button buttonSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAvatar = (Button) findViewById(R.id.buttonAvatar);
+        buttonQuests = (Button) findViewById(R.id.buttonQuests);
+        buttonSettings = (Button) findViewById(R.id.buttonSettings);
+
+        buttonAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AvatarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonQuests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AvatarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AvatarActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
