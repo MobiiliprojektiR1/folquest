a4b36dc00ab7a727d5ad015177cc3637a6a7be7f
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-03-21 13:46:11 +0200

    First Mock commit
    The first mockup for button placements, no functionality.

---
+++ Folquest/app/src/main/java/com/kantele/folquest/QuestBoardActivity.java
@@ -0,0 +1,13 @@
+package com.kantele.folquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_board);
+    }
+}
