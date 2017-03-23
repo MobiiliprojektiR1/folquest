cf33b1bd36f275e6f14652dcd6a978f1cdd80542
kahavi <teemu_kolu@hotmail.com>
2017-03-23 15:13:24 +0200

    Quest class merge
    Merged with quest class because I needed the class :poop:

--- Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
+++ Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
@@ -1,5 +1,7 @@
 package com.kantele.folquest;
 
+import java.util.ArrayList;
+
 /**
  * Created by Teemu on 23.3.2017.
  *
@@ -10,10 +12,34 @@ package com.kantele.folquest;
 public class PlayerController {
 
     //Variables
-    int playerGold;
-    int playerExp;
-
-    Quest
+    long playerGold;
+    long playerExp;
+    int maximumQuests = 3;
+    ArrayList<Quest> activeQuests = new ArrayList<>();
 
     //Methods
+    public long getPlayerGold() { return playerGold; }
+
+    public void setPlayerGold(long playerGold) { this.playerGold = playerGold; }
+
+    public long getPlayerExp() { return playerExp; }
+
+    public void setPlayerExp(long playerExp) { this.playerExp = playerExp; }
+
+    public void addQuest(Quest newQuest){
+        if(activeQuests.size() <= maximumQuests)
+            activeQuests.add(newQuest);
+    }
+
+    public void checkProgress(){
+        for(int i = 0; i < activeQuests.size() - 1; i++){
+            //progressBarMainNäkymässä.progress = activeQuests.get(i).getProgress();
+        }
+
+    }
+
+    public void completeQuest(int questIndex){
+        this.playerExp =+ activeQuests.get(questIndex).getRewardExp();
+        this.playerGold =+ activeQuests.get(questIndex).getRewardGold();
+    }
 }
--- Folquest/app/src/main/java/com/kantele/folquest/Quest.java
+++ Folquest/app/src/main/java/com/kantele/folquest/Quest.java
@@ -1,5 +1,7 @@
 package com.kantele.folquest;
 
+import android.content.res.Resources;
+
 /**
  * Created by Teemu on 22.3.2017.
  */
@@ -16,89 +18,94 @@ public class Quest{
         STEPS,
         WATER,
         CALORIES,
-        REST,
+        REST
     }
 
-    //Difficulty level 0-5        NOVICE,   EASY, NORMAL,   HARD, V.HARD,   EPIC
-    int pushUpDifficulty[]   =  {      5,     15,     30,     60,    200,    500 }; //reps
-    int sitUpDifficulty[]    =  {     10,     25,     50,    100,    500,   1000 }; //reps
-    int squatsDifficulty[]   =  {     10,     25,     50,    100,    500,   1000 }; //reps
-    int wallSitDifficulty[]  =  {     20,     60,    120,    300,   1500,   6000 }; //seconds
-    int distanceDifficulty[] =  {   2000,   5000,  10000,  20000, 100000, 200000 }; //meters
-    int stepsDifficulty[]    =  {   1500,   3000,   6000,  15000,  90000, 180000 }; //steps
-    int waterDifficulty[]    =  {      1,      8,     24,     56,    112,    224 }; //glasses of water for a day/day/3 days/week/2 weeks/month
-    int caloriesDifficulty[] =  {    500,   1500,   3000,   7000,  50000, 100000 }; //kilocalories burned
-    int restDifficulty[]     =  {      1,      3,      7,     14,     30,    180 }; //sleep at least 8 hours a day for this many days
+    //Difficulty level 0-5         NOVICE,   EASY, NORMAL,   HARD, V.HARD,   EPIC
+    long pushUpDifficulty[]   =  {      5,     15,     30,     60,    200,    500 }; //reps
+    long sitUpDifficulty[]    =  {     10,     25,     50,    100,    500,   1000 }; //reps
+    long squatsDifficulty[]   =  {     10,     25,     50,    100,    500,   1000 }; //reps
+    long wallSitDifficulty[]  =  {     20,     60,    120,    300,   1500,   6000 }; //seconds
+    long distanceDifficulty[] =  {   2000,   5000,  10000,  20000, 100000, 200000 }; //meters
+    long stepsDifficulty[]    =  {   1500,   3000,   6000,  15000,  90000, 180000 }; //steps
+    long waterDifficulty[]    =  {      1,      8,     24,     56,    112,    224 }; //glasses of water for a day/day/3 days/week/2 weeks/month
+    long caloriesDifficulty[] =  {    500,   1500,   3000,   7000,  50000, 100000 }; //kilocalories burned
+    long restDifficulty[]     =  {      1,      3,      7,     14,     30,    180 }; //sleep at least 8 hours a day for this many days
 
-    //Rewards based on difficulty NOVICE,   EASY, NORMAL,   HARD, V.HARD,   EPIC
-    int expRewards[]         =  {     50,    200,    500,   1250,   8000,  20000 };
-    int goldRewards[]        =  {     10,     50,    200,   1000,   4000,  20000 };
+    //Rewards based on difficulty  NOVICE,   EASY, NORMAL,   HARD, V.HARD,   EPIC
+    long expRewards[]         =  {     50,    200,    500,   1250,   8000,  20000 };
+    long goldRewards[]        =  {     10,     50,    200,   1000,   4000,  20000 };
 
     //Variables
     protected questType type;
     protected int difficultyLevel;
     protected int requirement;
     protected int progress;
-    protected int rewardGold;
-    protected int rewardExp;
+    protected long rewardGold;
+    protected long rewardExp;
     protected String description;
 
     //Methods
-    public Quest(questType newType, int newDifficultyLevel, int newGold, int newExp, String newDescription){
+    public Quest(questType newType, int newDifficultyLevel, long newGold, long newExp){
 
         type = newType;
         difficultyLevel = newDifficultyLevel;
         rewardGold = newGold;
         rewardExp = newExp;
-        description = newDescription;
+        progress = 0;
 
         switch (type){
             case PUSHUPS:
                 requirement = pushUpDifficulty[difficultyLevel];
-                //description = res.strings.getByYabadabadoo;
+                description = "Push up quest";
                 break;
             case SITUPS:
                 requirement = sitUpDifficulty[difficultyLevel];
+                description = "Sit up quest";
                 break;
             case SQUATS:
                 requirement = squatsDifficulty[difficultyLevel];
+                description = "Squat quest";
                 break;
             case WALLSIT:
                 requirement = wallSitDifficulty[difficultyLevel];
+                description = "Wall sit quest";
                 break;
             case DISTANCE:
                 requirement = distanceDifficulty[difficultyLevel];
+                description = "Distance quest";
                 break;
             case STEPS:
                 requirement = stepsDifficulty[difficultyLevel];
+                description = "Step quest";
                 break;
             case WATER:
                 requirement = waterDifficulty[difficultyLevel];
+                description = "Water quest";
                 break;
             case CALORIES:
                 requirement = caloriesDifficulty[difficultyLevel];
+                description = "Calorie quest";
                 break;
             case REST:
                 requirement = restDifficulty[difficultyLevel];
+                description = "Rest quest";
                 break;
         }
-
-        progress = 0;
     }
 
-    public int getRequirement(){
-        return requirement;
-    }
+    public long getRequirement() { return requirement; }
 
     public int getProgress(){
         return progress;
     }
+    public void setProgress( int newProgress ) { this.progress = newProgress; }
 
-    public int getRewardGold(){
+    public long getRewardGold(){
         return rewardGold;
     }
 
-    public int getRewardExp(){
+    public long getRewardExp(){
         return rewardExp;
     }
 
