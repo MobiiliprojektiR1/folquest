9afb13901154d0715586544b9fa88dcb1ab24646
kahavi <teemu_kolu@hotmail.com>
2017-03-23 11:10:39 +0200

    PlayerController class

---
+++ Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
@@ -0,0 +1,19 @@
+package com.kantele.folquest;
+
+/**
+ * Created by Teemu on 23.3.2017.
+ *
+ * This class keeps track of different player stats, their inventory,
+ * and handles some game functions
+ */
+
+public class PlayerController {
+
+    //Variables
+    int playerGold;
+    int playerExp;
+
+    Quest
+
+    //Methods
+}
