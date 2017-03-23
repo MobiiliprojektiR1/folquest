c4525ba67017905d6f3a8cc03a495136e72ca612
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-03-23 11:48:39 +0200

    Added functionality for Google Fit Sync

--- Folquest/app/src/main/java/com/kantele/folquest/MainActivity.java
+++ Folquest/app/src/main/java/com/kantele/folquest/MainActivity.java
@@ -1,16 +1,48 @@
 package com.kantele.folquest;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.result.DailyTotalResult;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonAvatar;
-    Button buttonQuests;
-    Button buttonSettings;
+    private static String TAG = "FIT:";
+    int EXPERIENCE_CURRENT, EXPERIENCE_TARGET;
+
+    Button buttonAvatar, buttonQuests, buttonSettings;
+
+    TextView textViewExpCurrent, textViewExpTarget;
+
+    TextView textViewStepsHolder, textViewSteps;
+    Button buttonUpdate;
+
+    // GOOGLE FIT
+
+    GoogleApiClient apiClient;
+
+    Boolean authInProgress;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +52,22 @@ public class MainActivity extends AppCompatActivity {
         buttonAvatar = (Button) findViewById(R.id.buttonAvatar);
         buttonQuests = (Button) findViewById(R.id.buttonQuests);
         buttonSettings = (Button) findViewById(R.id.buttonSettings);
+
+        textViewExpCurrent = (TextView) findViewById(R.id.textViewExpCurrent);
+        textViewExpTarget = (TextView) findViewById(R.id.textViewExpTarget);
+
+        textViewStepsHolder = (TextView) findViewById(R.id.textViewStepsHolder);
+        textViewSteps = (TextView) findViewById(R.id.textViewSteps);
+        buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+
+        EXPERIENCE_CURRENT = 1200;
+        EXPERIENCE_TARGET = 3000;
+
+        textViewExpCurrent.setText("" + EXPERIENCE_CURRENT);
+        textViewExpTarget.setText("" + EXPERIENCE_TARGET);
+
+
+        // BUTTON FUNCTIONALITIES
 
         buttonAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +93,116 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new FetchStepsAsync().execute();
+            }
+        });
+
+
+        // CREATE THE CONNECTION TO GOOGLE FIT
+
+        buildFitnessClient();
+
+        //PERMISSION REQUESTS
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // This ensures that if the user denies the permissions then uses Settings to re-enable
+        // them, the app will start working.
+        buildFitnessClient();
+    }
+
+    /**
+     *  Build a {@link GoogleApiClient} that will authenticate the user and allow the application
+     *  to connect to Fitness APIs. The scopes included should match the scopes your app needs
+     *  (see documentation for details). Authentication will occasionally fail intentionally,
+     *  and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
+     *  can address. Examples of this include the user never having signed in before, or having
+     *  multiple accounts on the device and needing to specify which account to use, etc.
+     */
+    private void buildFitnessClient() {
+        if (apiClient == null) {
+            apiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Fitness.HISTORY_API)
+                    .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
+                    .addConnectionCallbacks(
+                            new GoogleApiClient.ConnectionCallbacks() {
+                                @Override
+                                public void onConnected(Bundle bundle) {
+                                    Log.i(TAG, "Connected!!!");
+                                    // Now you can make calls to the Fitness APIs.
+                                    //Async To fetch steps
+                                }
+
+                                @Override
+                                public void onConnectionSuspended(int i) {
+                                    // If your connection to the sensor gets lost at some point,
+                                    // you'll be able to determine the reason and react to it here.
+                                    if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                        Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                    } else if (i
+                                            == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                        Log.i(TAG,
+                                                "Connection lost.  Reason: Service Disconnected");
+                                    }
+                                }
+                            }
+                    )
+                    .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+                            Log.i(TAG, "Google Play services connection failed. Cause: " +
+                                    result.toString());
+                            Snackbar.make(
+                                    MainActivity.this.findViewById(R.id.activity_main),
+                                    "Exception while connecting to Google Play services: " +
+                                            result.getErrorMessage(),
+                                    Snackbar.LENGTH_INDEFINITE).show();
+                        }
+                    })
+                    .build();
+        }
+    }
+
+
+    private class FetchStepsAsync extends AsyncTask<Object, Object, Long> {
+        protected Long doInBackground(Object... params) {
+            long total = 0;
+            PendingResult<DailyTotalResult> result = Fitness.HistoryApi.readDailyTotal(apiClient, DataType.TYPE_STEP_COUNT_DELTA);
+            DailyTotalResult totalResult = result.await(30, TimeUnit.SECONDS);
+            if (totalResult.getStatus().isSuccess()) {
+                DataSet totalSet = totalResult.getTotal();
+                if (totalSet != null) {
+                    total = totalSet.isEmpty()
+                            ? 0
+                            : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                }
+            } else {
+                Log.w(TAG, "There was a problem geting the step count.");
+            }
+
+            return total;
+        }
+
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            super.onPostExecute(aLong);
+
+            //Total steps covered for that day
+            Log.i(TAG, "Total steps: " + aLong);
+
+            textViewSteps.setText("" + aLong);
+        }
     }
 }
