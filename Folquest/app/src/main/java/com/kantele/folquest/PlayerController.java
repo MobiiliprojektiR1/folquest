e81b0128e8fbe0e3705a100a63eac4ef4b84c00b
kahavi <teemu_kolu@hotmail.com>
2017-04-06 09:07:45 +0300

    Save function toteutuksen aloitus

--- Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
+++ Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
@@ -1,6 +1,7 @@
 package com.kantele.folquest;
 
 import android.app.Application;
+import android.os.Environment;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -163,4 +164,29 @@ public class PlayerController extends Application{
     /**
      *Item Methods end
      */
+    public void save(){
+        if(isExternalStorageWritable()){
+
+        }
+    }
+
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
 }
