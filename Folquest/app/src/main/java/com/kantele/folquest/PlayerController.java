6c7e672fd99064960fda44201683fabd74432d67
Ville Marttila <t3mavi00@students.oamk.fi>
2017-04-12 12:52:28 +0300

    NOT WORKING accessory and feet added.
    Can’t get this work, somebody help?

--- Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
+++ Folquest/app/src/main/java/com/kantele/folquest/PlayerController.java
@@ -28,11 +28,12 @@ public class PlayerController extends Application{
 
 
     //Variables
-    public Boolean firstTimeState = true;
 
     /**
      * Item variables start
      */
+
+    public Boolean firstTimeState = false;
 
     ItemList itemList = new ItemList();
 
@@ -40,11 +41,13 @@ public class PlayerController extends Application{
     public ArrayList<Item> ownedTorsoItems = new ArrayList<>();
     public ArrayList<Item> ownedBottomItems = new ArrayList<>();
     public ArrayList<Item> ownedFeetItems = new ArrayList<>();
+    public ArrayList<Item> ownedAccessoryItems = new ArrayList<>();
 
     public Item equippedHeadItem;
     public Item equippedTorsoItem;
     public Item equippedBottomItem;
     public Item equippedFeetItem;
+    public Item equippedAccessoryItem;
     /**
      *Item variables end
      */
@@ -162,6 +165,14 @@ public class PlayerController extends Application{
         this.equippedFeetItem = equippedFeetItem;
     }
 
+    public Item getEquippedAccessoryItem() {
+        return equippedAccessoryItem;
+    }
+
+    public void setEquippedAccessoryItem(Item equippedAccessoryItem) {
+        this.equippedAccessoryItem = equippedAccessoryItem;
+    }
+
     private boolean inList(Item item){
         if(item.getItemType() == HEAD) {
             for (Item itemInList : ownedHeadItems) {
@@ -189,6 +200,14 @@ public class PlayerController extends Application{
         }
         if(item.getItemType() == FEET) {
             for (Item itemInList : ownedFeetItems) {
+                if (Objects.equals(item.getItemId(), itemInList.getItemId())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        if(item.getItemType() == OTHER) {
+            for (Item itemInList : ownedAccessoryItems) {
                 if (Objects.equals(item.getItemId(), itemInList.getItemId())) {
                     return true;
                 }
@@ -223,6 +242,12 @@ public class PlayerController extends Application{
                 ownedFeetItems.add(item);
             }
         }
+        else if(item.getItemType() == OTHER) {
+            if(!inList(item)) {
+                //Item is NOT the list, add it!
+                ownedAccessoryItems.add(item);
+            }
+        }
     }
 
     /* Adding defaults items when the game is started, these have to be in the database from the start! */
@@ -232,6 +257,7 @@ public class PlayerController extends Application{
         addItem(itemList.defaultTorso);
         addItem(itemList.defaultBottom);
         addItem(itemList.defaultFeet);
+        addItem(itemList.accessorySword);
     }
 
     /**
