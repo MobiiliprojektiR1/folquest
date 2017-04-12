dccf7a294b0ab064bda6cc860dfe4d55bf15aa43
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-04-12 15:04:12 +0300

    Revert "Revert "Saves first time launch and gender""
    This reverts commit 0dff67e51d8e7f1bef03cd84c41264249f0658bc.

--- Folquest/app/src/main/java/com/kantele/folquest/ItemList.java
+++ Folquest/app/src/main/java/com/kantele/folquest/ItemList.java
@@ -17,27 +17,25 @@ class ItemList {
     Item defaultTorso = new Item(TORSO, "default_torso", "Dirty rags", "Your default torso");
     Item defaultBottom = new Item(BOTTOM, "default_bottom", "Default Pants", "Your default bottom");
     Item defaultFeet = new Item(FEET, "default_feet", "Boots", "Your default boots");
-
     //  Item                            Type    Id                  Name            Description
     //  Item exampleItem = new Item(    HEAD,   "headExample",      "Example",      "Example Description");
 
-    // HEAD
+    Item headBeanie = new Item(HEAD, "headBeanie", "Beanie", "It reads Nutcase");
+    Item headHelmet = new Item(HEAD, "headHelmet", "Tough Helmet", "Forged deep in the mountain of forging");
     Item headBald   = new Item(HEAD, "head_bald", "Bald head", "Who needs helmet? or even hair");
-    Item headBoy = new Item(HEAD, "default_hair_man", "Male Default Hair", "Nice and simple");
-    Item headBunches = new Item(HEAD, "hair_bunches", "Ponytails", "Super cute and fearsome");
+    Item headSpikyHair = new Item(HEAD, "headSpikyHair", "Spiky Hair", "For true protagonist");
+    Item headTiara = new Item(HEAD, "headTiara", "Golden Tiara", "Belongs to a princess in a another castle");
 
-    // TORSO
-    Item torsoBlueWoman = new Item(TORSO, "shirt_blue_woman", "Blue Knight Armor", "Nice armor");
-    Item torsoBlueMan = new Item(TORSO, "shirt_man_blue", "Blue Knight Armor", "Nice armor");
+    Item torsoShirt = new Item(TORSO, "torsoShirt", "Shirt", "Basic Shirt");
+    Item torsoTest = new Item(TORSO, "test_shirt", "Chainmail", "Tough");
+    Item torsoRobe = new Item(TORSO, "torsoRobe", "Orange Robe", "Munk robe");
+    Item torsoSpikyShirt = new Item(TORSO, "torsoSpikyShirt", "Spiky Shirt", "Spiky Shirt");
+    Item torsoPinkDress = new Item(TORSO, "torsoPinkDress","Princess Dress", "pink dress");
 
-    //BOTTOM
-    Item bottomBlueTrousers = new Item(BOTTOM, "legs_blue", "Jeans", "Jeans.");
-
-    // FEET
-    Item feetBlackBoots = new Item(FEET, "feet_black_boots", "Black Boots", "Nice and casual");
-
-    // ACCESSORIES
-    Item accessorySword = new Item(OTHER, "accessory_sword", "Sword", "Slay those Dragon");
-    Item accessoryNone = new Item(OTHER, "none", "No accessory", "No accessory");
+    Item bottomTest = new Item(BOTTOM, "test_legs", "Black Pants", "Pants");
+    Item bottomSabatons  = new Item(BOTTOM, "bottomSabatons", "Knights boots", "Sabatons");
+    Item bottomSandals = new Item(BOTTOM, "bottomSandals", "Sandals", "Socks and sandals");
+    Item bottomSpikyLegs = new Item(BOTTOM, "bottomSpikyLegs", "Spiky leggings", "Yeah");
+    Item bottomHighHeels = new Item(BOTTOM, "bottomHighHeels", "High Heels", "Also pink");
 
 }
